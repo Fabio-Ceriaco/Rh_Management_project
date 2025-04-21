0681bf2cdc76813b93b1e781d9ab3970f6fe65e6
Fabio-Ceriaco <ceriacofabio@gmail.com>
2025-04-21 09:34:27 +0100

    Mailhog configuration

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use Illuminate\Mail\Message;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/email', function () {
+    Mail::raw('test message rh', function (Message $message) {
+        $message->to('test@gmail.com')
+            ->subject('Welcome to Rh_Management')
+            ->from('rh@rh_management.com');
+    });
+    echo 'OK';
 });
